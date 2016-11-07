3efdf03af085c1aa4dd9a0ddebbb081f707acb50
aokad <aokada@ims.u-tokyo.ac.jp>
2016-11-07 15:36:50 +0900

    Update run_pmsignature_full.R

--- pmsignature/run_pmsignature_full.R
+++ pmsignature/run_pmsignature_full.R
@@ -18,17 +18,11 @@ sigNum <- as.numeric(commandArgs()[7]);
 trDirFlag <- as.logical(commandArgs()[8]);
 trialNum <- as.numeric(commandArgs()[9]);
 
-#G <- readMPFile(inputFile, numBases = 5, trDir = trDirFlag);
-G <- readMPFile(inputFile, numBases = 3, type = "full", trDir = FALSE);
-
+G <- readMPFile(inputFile, numBases = 3, type = "full", trDir = trDirFlag);
 BG_prob <- readBGFile(G);
-
-#Param <- getPMSignature(G, K = sigNum, BG = BG_prob, numInit = trialNum);
-Param <- getPMSignature(G, K = 3, BG = BG_prob, numInit=10)
-
+Param <- getPMSignature(G, K = sigNum, BG = BG_prob, numInit = trialNum);
 Boot <- bootPMSignature(G, Param0 = Param, bootNum = 100, BG = BG_prob);
 
-#save(list(Param, Boot), file=outputFile);
 resultForSave <- list(Param, Boot);
 save(resultForSave, file=outputFile);
 
